df48608f1993fbe0c5bcd48197866e1fd0603442
Egor Gorokhov <egorgorokhovmn@gmail.com>
2020-08-10 21:03:30 +0300

    Add funcs to get available moves for King and Rook

--- src/Chess.scala
+++ src/Chess.scala
@@ -1,23 +1,81 @@
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 import scala.io.StdIn
 
 class Player(var color: Int) {
   // 1 = white, -1 = black
   require(color == -1 || color == 1)
 
-  // Map with the names of figures(Value) and their numbers(Key)
-  val figures: mutable.Map[Int, String] = mutable.Map(
-    1 ->"King",
-    2 -> "Queen",
-    3 -> "Rook",
-    4 -> "Rook",
-    5 -> "Knight",
-    6 -> "Knight",
-    7 -> "Bishop",
-    8 -> "Bishop"
-  ) ++ (for (n <- 9 to 16) yield (n, "Pawn")).toMap
+  private val initialRowPos = if (color == -1) 0 else 7
 
-  val availableMoves: mutable.Map[Int, Array[(Int, Int)]] = mutable.Map()
+  // Number of fig -> Name, Position
+  val figures: mutable.Map[Int, (String, (Int, Int))] = mutable.Map(
+    1 -> ("King", (initialRowPos, 3)),
+    2 -> ("Queen", (initialRowPos, 4)),
+    3 -> ("Rook", (initialRowPos, 0)),
+    4 -> ("Rook", (initialRowPos, 7)),
+    5 -> ("Knight", (initialRowPos, 1)),
+    6 -> ("Knight", (initialRowPos, 6)),
+    7 -> ("Bishop", (initialRowPos, 2)),
+    8 -> ("Bishop", (initialRowPos, 5))
+  ) ++ (for (i <- 0 to 7) yield (i + 9, ("Pawn", (initialRowPos - color, i)))).toMap
+
+  val availableMoves: mutable.Map[Int, List[(Int, Int)]] = mutable.Map()
+
+  def getAvailableMoves(figName: Int, pos: (Int, Int), board: Array[Array[Int]]): ListBuffer[(Int, Int)] = {
+
+    // TODO Write functions for every fig class for getting available positions
+
+    def fKing(pos: (Int, Int)) = {
+      val positions = new ListBuffer[(Int, Int)]()
+      val rowBounds = Array(pos._1 - 1, pos._1 + 1)
+      rowBounds.foreach(row => {
+        if (board(2)(row) == 0) positions += Tuple2(2, row)
+        for (col <- pos._2 - 1 to pos._2 + 1) if (board(row)(col) == 0) positions += Tuple2(row, col)
+      })
+      positions
+    }
+
+    def fRook(pos: (Int, Int), board: Array[Array[Int]]) = {
+      val positions = new ListBuffer[(Int, Int)]()
+
+      // Scan vertically to detect obstacles
+      def scanOnRows() = {
+        // Need to check both sides of current pos:
+        // towards 0 and towards 7
+        Array(0, 7).foreach(endPoint => {
+          val order = if (endPoint == 0) -1 else 1  // Use this val to be able iterate in ascending and descending orders
+          var row = pos._1 + order  // Don't scan start position
+          var noObstacle = true
+          // Iterating over constant column
+          while (noObstacle && (row * order <= endPoint)) {
+            positions += Tuple2(row, pos._2)
+            noObstacle = (board(row)(pos._2) == 0)
+            row += order
+          }})}
+
+      // Scan horizontally
+      def scanOnCols() = {
+        // Doing the same as scanOnRows but row is constant
+        Array(0, 7).foreach(endPoint => {
+          val order = if (endPoint == 0) -1 else 1
+          var col = pos._2 + order
+          var noObstacle = true
+          while (noObstacle && (col * order <= endPoint)) {
+            positions += Tuple2(pos._1, col)
+            noObstacle = (board(pos._1)(col) == 0)
+            col += order
+          }})}
+
+      scanOnRows()
+      scanOnCols()
+      positions
+    }
+
+  }
+}
+
+
 
 }
 
@@ -53,34 +111,15 @@ class Game {
 
   // Board represented as a 2d array, white figs down, black up.
   // 1 = white, -1 = black
-  def initBoard(): Array[Array[Int]] = {
+  def initBoard(players: Array[Player]): Array[Array[Int]] = {
     val board = Array.ofDim[Int](8, 8)
 
-    // Pawns initialization
-    for (i <- 1 to board.length) {
-      board(1)(i) = -(i + 8)
-      board(6)(i) = i + 8
-    }
-
-    for (i <- Array(0, 7)) {
-      val side = if (i == 0) -1 else 1
-      //King
-      board(i)(3) = 1*side
-
-      //Queen
-      board(i)(4) = 2*side
-
-      //Rooks
-      board(i)(0) = 3*side
-      board(i)(7) = 4*side
-
-      //Knights
-      board(i)(1) = 5*side
-      board(i)(6) = 6*side
-
-      //Bishops
-      board(i)(2) = 6*side
-      board(i)(5) = 7*side
+    for (player <- players) {
+      player.figures.foreach(pair => {
+        val pos = pair._2._2
+        val numFig = pair._1
+        board(pos._1)(pos._2) = numFig
+        })
     }
     board
   }
